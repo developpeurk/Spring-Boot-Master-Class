bcc7021887322d427ac54f1afc4c37f13b7bec31
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-19 13:56:16 +0100

    javax.validation

--- src/main/java/com/example/demo/customer/CustomerControllerV2.java
+++ src/main/java/com/example/demo/customer/CustomerControllerV2.java
@@ -2,7 +2,7 @@ package com.example.demo.customer;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -27,13 +27,13 @@ public class CustomerControllerV2 {
     }
 
     @PostMapping
-    void createNewCustomer(@RequestBody Customer customer){
+    void createNewCustomer(@Valid @RequestBody Customer customer){
         System.out.println("POST REQUEST ...");
         System.out.println(customer);
     }
 
     @PutMapping
-    void updateCustomer(@RequestBody Customer customer){
+    void updateCustomer(@Valid @RequestBody Customer customer){
         System.out.println("UPDATE REQUEST ...");
         System.out.println(customer);
     }
--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -7,9 +7,9 @@ public class CustomerFakeRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
         return Arrays.asList(
-                new Customer(1L, "James Bond", "password123"),
-                new Customer(2L, "Jamila Bond", "password123"),
-                new Customer(3L, "Ahmed Bond", "password123")
+                new Customer(1L, "James Bond", "password123", "email@gmail.com"),
+                new Customer(2L, "Jamila Bond", "password123", "email@gmail.com"),
+                new Customer(3L, "Ahmed Bond", "password123", "email@gmail.com")
         );
     }
 }
