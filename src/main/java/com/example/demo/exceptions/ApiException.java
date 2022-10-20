8b5273985b928f061835c62ad446769888330920
developpeurk <developpeurk@gmail.com>
2022-10-20 13:40:45 +0100

    record

--- src/main/java/com/example/demo/exceptions/ApiException.java
+++ src/main/java/com/example/demo/exceptions/ApiException.java
@@ -5,11 +5,8 @@ import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@Data
-public class ApiException {
-    private final String message;
-    private final Throwable throwable;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime zonedDateTime;
-
+public record ApiException(String message,
+                           Throwable throwable,
+                           HttpStatus httpStatus,
+                           ZonedDateTime zonedDateTime) {
 }
